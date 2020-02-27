9142dbf09c9b7a5fecdb0ec528ad2d4f38758af7
Sergio G <esgonzalezca@unal.edu.co>
2020-02-26 23:13:31 -0500

    final commit

---
+++ Augmented reality App/KidKitAppV2/Assets/scripts/Escanear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Escanear : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+    public string escena;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(escena);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Augmented reality App/Readme.txt
@@ -0,0 +1,4 @@
+1-Install kitkidapp.apk on Android mobile
+2-When running, focus the camera on one model in folder "Models"
+
+Unity project Files "KidKitAppV2"
